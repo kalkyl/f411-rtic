9058c40b73a0cbd4b84d6895874922bc1db24d77
Henrik Alsér <henrik.alser@me.com>
2021-08-06 17:39:15 +0200

    Cleanup

--- src/bin/serial-dma-loopback.rs
+++ src/bin/serial-dma-loopback.rs
@@ -156,7 +156,7 @@ mod app {
         accumulate::spawn(Vec::from_slice(data).unwrap()).ok();
     }
 
-    #[task(local = [cobs_buf: CobsAccumulator<64> = CobsAccumulator::new()], shared = [status, handle], priority = 1, capacity = 2)]
+    #[task(local = [cobs_buf: CobsAccumulator<16> = CobsAccumulator::new()], shared = [status, handle], priority = 1, capacity = 2)]
     fn accumulate(ctx: accumulate::Context, data: Vec<u8, BUF_SIZE>) {
         match ctx.local.cobs_buf.feed::<Command>(data.as_slice()) {
             FeedResult::Success { data: command, .. } => {
